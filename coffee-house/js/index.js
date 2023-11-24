992c59615d76bee87ccbe979f7cca06077a36abf
YuliyaVoronovich <Y-1306@mail.ru>
2023-11-24 18:08:18 +0300

    feat: add burger-menu realization

---
+++ coffee-house/js/index.js
@@ -0,0 +1,19 @@
+//бургер-меню
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('burger').addEventListener('click', function() {    
+
+        document.querySelector('.header').classList.toggle('open');
+        document.querySelector('body').classList.toggle('body-overflow');      
+    })
+});
+
+const links = Array.from(document.querySelector('.nav-list').children);
+
+ links.forEach((link) => {
+    link.addEventListener("click", closeOnClick);
+});
+
+function closeOnClick() {
+     document.querySelector('.header').classList.remove('open');
+     document.querySelector('body').classList.remove('body-overflow');
+ }
