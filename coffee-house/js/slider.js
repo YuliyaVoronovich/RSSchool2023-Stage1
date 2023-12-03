1b6c9ae4e4a91dfea98ffea67318caf86e6a40a3
YuliyaVoronovich <Y-1306@mail.ru>
2023-12-03 23:49:14 +0300

    feat: add progreebar to slider

--- coffee-house/js/slider.js
+++ coffee-house/js/slider.js
@@ -1,38 +1,59 @@
-let autoplayInterval = null;
+//let autoplayInterval;
+let progressInterval;
 const startPosition = 100;
 let movePosition;
 
 let step = 0;
 let endStep = 2;
+let progressStep = 0;
+let predBar = 0;
+
+let currentSlide = 0;
 
 const container = document.querySelector('.favorites-slides');
 const track = document.querySelector('.favorites-list');
 const nextArrow = document.querySelector('.favorites-button-right');
 const prevArrow = document.querySelector('.favorites-button-left');
 
+const progressBarButtons = document.querySelectorAll('.carousel-button');
+
 function startSlider() {
-    if (!autoplayInterval) {
-      autoplayInterval = setInterval(nextSlide, 5000);
-    }
+   // stopSlider();
+   // autoplayInterval = setInterval(nextSlide, 4000);  
+    progressInterval = setInterval(progressBar, 200);
 }
 
 function stopSlider() {
-    clearInterval(autoplayInterval);
-    autoplayInterval = null;
+   // clearInterval(autoplayInterval);
+    clearInterval(progressInterval);
+}
+
+const progressBar = () => {
+
+    if (progressStep >= 100) {
+        progressStep = 0;
+        progressBarButtons[step].style.width = `0%`;
+        nextSlide();
+    } else {
+        progressStep +=4;
+        progressBarButtons[step].style.width = `${progressStep}%`;
+    }
+   
 }
 
 const nextSlide = () => {
 
     if (step >= endStep) {        
         step = 0;
-        track.style.transform = `translateX(-${0}%)`;
+        currentSlide = endStep;
+        track.style.transform = `translateX(${0}%)`;
     } else {
         step += 1; 
         movePosition = startPosition * step;
         track.style.transform = `translateX(-${movePosition}%)`;
     }    
-    startSlider();
 }
+
 const prevSlide = () => {
 
     if (step === 0) {  
@@ -44,21 +65,43 @@ const prevSlide = () => {
         movePosition = startPosition * step;
         track.style.transform = `translateX(-${movePosition}%)`;
     }    
-    startSlider();
 }
 
-nextArrow.addEventListener ('click',  event => {  
+nextArrow.addEventListener ('click',  event => { 
+
+    progressBarButtons[step].style.width = `0%`;
+    progressStep = 0;
     stopSlider();
-    startSlider();
-    nextSlide();
+    startSlider();  
+    nextSlide();  
 });
 
-prevArrow.addEventListener ('click',  event => {  
+prevArrow.addEventListener ('click',  event => {     
+     
+    progressBarButtons[step].style.width = `0%`;
+    progressStep = 0;
     stopSlider();
-    startSlider();
+    startSlider();    
     prevSlide();
+   
 });
+
+container.addEventListener ('mouseover',  event => { 
+    stopSlider();   
+});
+
+container.addEventListener ('mouseout',  event => {    
+    startSlider();   
+});
+
+// container.addEventListener ('mouseup',  event => { 
+//     stopSlider();   
+// });
+
+// container.addEventListener ('mousedown',  event => {    
+//     startSlider();   
+// });
 
 window.addEventListener('load', () => {
-    startSlider();
+    startSlider(); 
   });
