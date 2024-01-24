b7881919ac46613b9cce8542c953044e50cf8bac
YuliyaVoronovich <Y-1306@mail.ru>
2024-01-24 13:57:13 +0300

    feat: add component table for game

--- nonograms/js/game.js
+++ nonograms/js/game.js
@@ -2,9 +2,38 @@ import cards from "./matrix.json" assert { type: "json" };
 
 class Game {
 
-  constructor () {
+  constructor(level = 'basic', name = 'Name1') {
+    this.card;
+    this.level = level;
+    this.name = name;
+    this.matrix;
+  }
 
-  }  
+  getCard() {
+    return this.card = cards.filter((item) => item.level === this.level)
+                            .filter((item) => item.name === this.name)[0];
+  };
+
+  getArrayFromMatrix() {
+    let result = [];
+    const array = this.card.matrix.split(',');
+    for (let i = 0; i < array.length; i++) {
+      result[i] = [];
+      array[i] = array[i].slice(1, -1);
+      for (let j = 0; j < array[i].length; j++) {
+        result[i][j]=array[i][j];
+      }
+    }
+    return result;
+  }
+
+  getCurrentMatrix() {
+    this.matrix = this.getArrayFromMatrix();
+  };
+
+  getLengthMatrix() {
+    return this.matrix.length;
+  };
 }
 
 export default Game;
--- nonograms/js/main.js
+++ nonograms/js/main.js
@@ -3,7 +3,45 @@ import Game from './game.js';
 
 import cards from "./matrix.json" assert { type: "json" };
 
- // ========= LAYOUT
+const card = new Game();
+card.getCard();
+card.getCurrentMatrix();
+console.log(card.matrix);
+
+
+ // ========= TABLE
 const body = document.querySelector('body');
 const main = new CreateElement('main', ['main']);
 body.append(main.element);
+
+const table = new CreateElement('table', ['table']);
+main.element.append(table.element);
+
+const thead = new CreateElement('thead');
+table.element.append(thead.element);
+
+const theadTr = new CreateElement('tr');
+thead.element.append(theadTr.element);
+
+const theadTh = new CreateElement('th', ['head']);
+theadTr.element.append(theadTh.element);
+
+for (let i = 0; i < card.getLengthMatrix(); i++) {
+  const theadTh = new CreateElement('th', ['line']);
+  theadTr.element.append(theadTh.element);
+}
+const tbody = new CreateElement('tbody');
+table.element.append(tbody.element);
+
+for (let i = 0; i < card.getLengthMatrix(); i++) {
+  const tbodyTr = new CreateElement('tr');
+  tbody.element.append(tbodyTr.element); 
+
+  const tbodyTd = new CreateElement('th', ['line']);
+  tbodyTr.element.append(tbodyTd.element);
+
+  for (let j = 0; j < card.getLengthMatrix(); j++) {
+    const tbodyTd = new CreateElement('td');
+    tbodyTr.element.append(tbodyTd.element);
+  }
+}
