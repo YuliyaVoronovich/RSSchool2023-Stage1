777186e0e9f77b247dfea8fb7956794150aa9a63
YuliyaVoronovich <Y-1306@mail.ru>
2024-01-25 11:13:07 +0300

    feat: add 4 buttons to game

--- nonograms/js/main.js
+++ nonograms/js/main.js
@@ -24,8 +24,22 @@ main.element.append(aside.element);
 const gameField = new CreateElement('div', ['game-field']);
 main.element.append(gameField.element);
 
+ // ========= BUTTONS
 const buttonsGame = new CreateElement('div', ['buttons-field']);
 gameField.element.append(buttonsGame.element);
+
+const buttonSolution = new CreateElement('button', ['button'], 'Solution');
+buttonsGame.element.append(buttonSolution.element);
+
+const buttonSave = new CreateElement('button', ['button'], 'Save');
+buttonsGame.element.append(buttonSave.element);
+
+const buttonContinue = new CreateElement('button', ['button'], 'Continue');
+buttonsGame.element.append(buttonContinue.element);
+
+const buttonReset = new CreateElement('button', ['button'], 'Reset');
+buttonsGame.element.append(buttonReset.element);
+
 
  // ========= TABLE
 gameField.element.append(tableWrap.element);
