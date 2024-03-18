c7584451e3a228fecef111daf55ca3688a9df027
YuliyaVoronovich <Y-1306@mail.ru>
2024-03-18 09:29:11 +0300

    Updates

--- rss-puzzle/assets/button-COblRQJx.js
+++
@@ -1,1 +0,0 @@
-import{B as o}from"./index-85cEIH6-.js";class n extends o{constructor({className:s,textContent:e,onClick:t}){super({tagName:"button",className:s,textContent:e}),t&&this.addListener("click",t)}}export{n as B};
