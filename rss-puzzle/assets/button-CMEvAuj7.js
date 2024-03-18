6ca07079dd79751ce32babbcc5312b6f142bf686
YuliyaVoronovich <Y-1306@mail.ru>
2024-03-18 14:57:45 +0300

    Updates

--- rss-puzzle/assets/button-CMEvAuj7.js
+++
@@ -1,1 +0,0 @@
-import{B as o}from"./index-0Zp8XGIa.js";class n extends o{constructor({className:s,textContent:e,onClick:t}){super({tagName:"button",className:s,textContent:e}),t&&this.addListener("click",t)}}export{n as B};
