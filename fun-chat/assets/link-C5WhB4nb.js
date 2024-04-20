684b42501cb5f138513574b3fa86c012f317be4b
YuliyaVoronovich <Y-1306@mail.ru>
2024-04-20 08:22:56 +0300

    Updates

---
+++ fun-chat/assets/link-C5WhB4nb.js
@@ -0,0 +1,1 @@
+import{B as e}from"./index-B4yvVzlv.js";class p extends e{constructor({className:s,href:o,textContent:t,target:a}){super({tag:"a",className:s,textContent:t,href:o,target:a})}}export{p as L};
