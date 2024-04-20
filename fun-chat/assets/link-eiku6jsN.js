a9e7674154ef2f583f76e576d13ac010aefe8774
YuliyaVoronovich <Y-1306@mail.ru>
2024-04-20 08:17:45 +0300

    Updates

---
+++ fun-chat/assets/link-eiku6jsN.js
@@ -0,0 +1,1 @@
+import{B as e}from"./index-C4x99KUs.js";class p extends e{constructor({className:s,href:o,textContent:t,target:a}){super({tag:"a",className:s,textContent:t,href:o,target:a})}}export{p as L};
--- fun-chat/assets/link-xBH1X7Er.js
+++
@@ -1,1 +0,0 @@
-import{B as e}from"./index-quXyQak6.js";class p extends e{constructor({className:s,href:o,textContent:t,target:a}){super({tag:"a",className:s,textContent:t,href:o,target:a})}}export{p as L};
